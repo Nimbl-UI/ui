ee86475ba9670c225ceeeecf97d327b521acd036
Cloyd Abad <abadcloyd@gmail.com>
2024-09-13 18:07:44 +0800

    setting up styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
+import css from "./styles.module.css";
 
-const Button: React.FC<Parameters> = ({children, ...rest}) => {
-	return (
-		<button {...rest}>
-			{children}
-		</button>
-	)
-}
+const Button: React.FC<Parameters> = ({ children, ...rest }) => {
+  return (
+    <button className={css.wrapper} {...rest}>
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
 
 interface Parameters {
-	children: any;
+  children: any;
 }
